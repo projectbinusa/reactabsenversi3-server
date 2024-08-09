ee6be42777194b55648ae0b8894477d04fbea6e9
Ave ceena17 <mingave11@gmail.com>
2024-08-09 09:04:12 -0700

    membuat editFoto dan editEmailDanName role ortu

--- src/main/java/com/example/absensireact/service/OrangTuaService.java
+++ src/main/java/com/example/absensireact/service/OrangTuaService.java
@@ -1,7 +1,11 @@
 package com.example.absensireact.service;
 
 import com.example.absensireact.model.OrangTua;
+import com.example.absensireact.model.SuperAdmin;
+import com.example.absensireact.model.User;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,4 +22,8 @@ public interface OrangTuaService {
     OrangTua tambahOrangTua(Long idSuperAdmin, OrangTua orangTua);
 
     void deleteOrangTua(Long id);
+
+    OrangTua uploadImage(Long id, MultipartFile image) throws IOException;
+
+    OrangTua ubahUsernamedanemail(Long id, OrangTua updateOrangTua);
 }

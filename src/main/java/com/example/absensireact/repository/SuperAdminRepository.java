33bdd5232a23d1a9ca9f0fba5fbd863f5e25df92
ZiyanHaidar <ziyanhaidar809@gmail.com>
2024-08-16 09:49:58 +0700

    menambahkan alert pada profile superadmin

--- src/main/java/com/example/absensireact/repository/SuperAdminRepository.java
+++ src/main/java/com/example/absensireact/repository/SuperAdminRepository.java
@@ -14,12 +14,11 @@ public interface SuperAdminRepository  extends JpaRepository<SuperAdmin , Long> 
     @Query(value = "SELECT * FROM super_admin WHERE email = :email", nativeQuery = true)
     Optional<SuperAdmin> findByEmail (String email);
 
-    @Query(value = "SELECT * FROM super_admin WHERE username = :username", nativeQuery = true)
-    boolean existsByUsername (String username);
 
     @Query(value = "SELECT * FROM super_admin WHERE username = :username", nativeQuery = true)
     Optional<SuperAdmin> findByUsername (String username);
     Boolean existsByEmail(String email);
+    Boolean existsByUsername(String username);
 
     Optional<SuperAdmin> findByEmailAndUsername(String email, String username);
 
